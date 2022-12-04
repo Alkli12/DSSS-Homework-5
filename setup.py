f5ffdf1655665681e6d369afa1251e9c8a23a7b5
Alkli12 <alexander.klingebiel@fau.de>
2022-12-04 21:31:44 +0100

    pipready

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='snowflake',
+    version='0.0.1',
+    author='Alex K',
+    description='Testing installation of Package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['snowflake'],
+    install_requires=['requests'],
+)
